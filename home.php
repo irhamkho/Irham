5509840f89a188db10bb7a47d0378abb41a2a6b1
irhamkho <70144641+irhamkho@users.noreply.github.com>
2020-08-31 12:37:24 +0700

    Add files via upload

--- home.php
+++ home.php
@@ -1,32 +1,31 @@
 <?php
-class japlak{
-  
-    var $pemesan;
-    var $jenis;
-    var $level;
-  
-   function tampil_matang() {
-     return "Japlak Sudah Matang";
-    }
-   function tampil_mentah() {
-     return "Japlak Belum Matang";
-   }
-}
-  
-$japlak1 = new japlak();
-  
-$japlak1->pemesan="Irham Khoeruamani";
-$japlak1->jenis="Japlak";
-$japlak1->level="Level 15";
-  
-echo $japlak1->pemesan;
-echo "<br />";
-echo $japlak1->jenis;
-echo "<br />";
-echo $japlak1->level;
-echo "<br />";
-  
-echo $japlak1->tampil_matang();
-echo "<br />";
-echo $japlak1->tampil_mentah();
+	class produk{
+	   public $namabarang,
+	          $merk,
+	          $harga;
+
+		public function getcetak(){
+			return"$this->namabarang, $this->merk, $this->harga";
+		}
+		public function __construct($namabarang="nama barang",$merk="merk",$harga=0){
+			$this->namabarang =$namabarang;
+			$this->merk =$merk;
+			$this->harga =$harga;
+		}
+	}
+		$produk1 = new produk("Idepad 310", "lenovo", 700000);
+		$produk2 = new produk("Flashdisk", "Sandisk", 100000);
+		$produk3 = new produk("Macebook Air", "Apple", 250000000);
+		$produk4 = new produk("Mouse", "Logitech", 1200000);
+
+	echo "Nama Barang Leptop : " . $produk1->getcetak();
+	echo "<br>" ;
+	echo "Nama Barang Aksesoris: " . $produk2->getcetak();
+	echo "<br>";
+	echo "Nama Barang Leptop : " . $produk3->getcetak();
+	echo "<br> ";
+	echo "Nama Barang Aksesoris : " . $produk4->getcetak();
 ?>
+	
+	  
+	  
